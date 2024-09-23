cd1dd07bf4ec9fd3c7d962ed98bc883f19b7ba58
balin <riccardo.balin@colorado.edu>
2024-09-23 17:15:29 +0000

    Added Adios debug env variable to run script

--- SimAI_Bench/adios2_example/run_workflow_sst.sh
+++ SimAI_Bench/adios2_example/run_workflow_sst.sh
@@ -9,6 +9,8 @@ module load spack-pe-gnu
 module load adios2/2.10.0-cuda
 source /eagle/datascience/balin/ALCF-4/venv/_adios2/bin/activate
 
+#export SstVerbose=1
+
 mpiexec -n $PROCS python simulation.py &
 mpiexec -n $PROCS python train.py
 
