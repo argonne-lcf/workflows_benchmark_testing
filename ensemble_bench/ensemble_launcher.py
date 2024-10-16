f40b90ffdf53757b444d18578dc54d3d4f9850dc
Christine Simpson <csimpson@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-10-15 21:59:33 -0500

    added lammps

--- ensemble_bench/ensemble_launcher.py
+++ ensemble_bench/ensemble_launcher.py
@@ -6,26 +6,46 @@ def launch_task(task,assigned_nodes):
     print(f"Launching task on nodes {assigned_nodes}")
 
     num_nodes = len(assigned_nodes)
-    ppn = 12
+    ppn = task["ranks_per_node"]
     nranks = num_nodes*ppn
+
+    cmd = task["cmd"]
+    run_dir = task["run_dir"]
 
     hosts_str = ""
     for node in assigned_nodes:
         if node != assigned_nodes[0]:
             hosts_str += ","
         hosts_str += node
-    cmd = f"mpiexec -n {nranks} --ppn {ppn} --hosts {hosts_str} sleep 10"
+
+    os.makedirs(run_dir, exist_ok=True)
+    cmd = f"cd {run_dir} & mpiexec -n {nranks} --ppn {ppn} --hosts {hosts_str} {mpi_options} {cmd} >> {run_dir}/job.out"
     p = subprocess.Popen(cmd, shell=True)
 
     return p
     
 ntasks = 10
+NX = 400
+gpu_affinity = "/soft/tools/mpi_wrapper_utils/gpu_tile_compact.sh"
+executable = "$HOME/bin/lmp_aurora_kokkos"
+inputs = f"-in $HOME/bin/lj_lammps_template.in -k on g 1 -var nx {NX} -sf kk -pk kokkos neigh half neigh/qeq full newton on"
+
+num_nodes = 2
+ranks_per_node =  12
+NDEPTH = 8
+NTHREADS = 1
+mpi_options = f"--depth={NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS={NTHREADS} --env OMP_PROC_BIND=spread --env OMP_PLACES=cores"
+
 
 # Make simple task list
 tasks = {}
 for i in range(ntasks):
-    tasks[str(i)] = { 'type':'lammps',
-                      'num_nodes': 2,
+    tasks[str(i)] = { 'name':'lammps',
+                      'cmd': f'{gpu_affinity} {executable} {inputs}',
+                      'num_nodes': num_nodes,
+                      'ranks_per_node': ranks_per_node,
+                      'mpi_options': mpi_options,
+                      'run_dir': os.getcwd()+f"outputs/{i}"
                       'status': 'ready'}
 
 # Get available nodes
