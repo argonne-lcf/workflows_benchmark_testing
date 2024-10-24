db90e581675deaaa65afb363a818bd59e6dcc843
balin <riccardo.balin@colorado.edu>
2024-10-24 21:53:44 +0000

    Fixed bug in GMRES affecting convergence, now GMRES gives exact solution after N iterations

--- SimAI_Bench/src/utils/sim_utils.py
+++ SimAI_Bench/src/utils/sim_utils.py
@@ -1,5 +1,5 @@
 import sys
-from time import sleep
+from time import sleep, perf_counter
 import numpy as np
 import math
 from mpipartition import Partition
@@ -141,7 +141,7 @@ def simulation_step(step: int, problem_size: str, coords: np.ndarray):
 
 
 # GMRES
-def gmres(A, b, x0=None, P=None, tol=1e-5, max_iter=100, restart=None):
+def gmres(A, b, x0=None, P=None, tol=1e-5, max_iter=200, restart=None, logging=False):
     """Solve the linear system Ax=b via Generalized Minimal RESidual
     
     Implemented in PyTorch
@@ -156,7 +156,8 @@ def gmres(A, b, x0=None, P=None, tol=1e-5, max_iter=100, restart=None):
     dtype = A.dtype
     np_dtype = torch.zeros((1,),dtype=dtype).numpy().dtype
     device = A.device
-    
+    if logging: print(f'Executing GMRES with precision {dtype} and on device {device}',flush=True)   
+ 
     if n == 0:
         return torch.zeros_like(b), 0, 0
     
@@ -165,21 +166,57 @@ def gmres(A, b, x0=None, P=None, tol=1e-5, max_iter=100, restart=None):
         return b, b_borm, 0
     
     if restart is None:
-        n_krylov = max_iter
-    else:
-        n_krylov = min(restart, max_iter)
+        restart = max_iter
+    n_krylov = min(n, min(restart, max_iter))
     assert n_krylov>=2, 'Number of max_iter or restart must be >= 2'
+    if logging: print(f'Using {n_krylov} Krylov vectors',flush=True)   
 
-    A, x0, b = make_system(A, P, x0, b)
+    A, P, x, b = make_system(A, P, x0, b)
 
+    # consider switching rows and columns of Q and H for performance
     Q = torch.empty((n, n_krylov+1), dtype=dtype, device=device)
     H = torch.zeros((n_krylov+1, n_krylov), dtype=dtype, device=device)
     e = torch.zeros((n_krylov+1,), dtype=dtype, device=device)
 
+    # Following cupy implementation
+    # https://github.com/cupy/cupy/blob/118ade4a146d1cc68519f7f661f2c145f0b942c9/cupyx/scipy/sparse/linalg/_iterative.py#L92
+    iters = 0
+    while True:
+        px = torch.matmul(P,x)
+        res = b - torch.matmul(A,px)
+        res_norm = torch.linalg.norm(res)
+        if res_norm <= tol or iters >= max_iter:
+            break
+    
+        q = res / res_norm
+        Q[:, 0] = q
+        e[0] = res_norm
+        for j in range(n_krylov):
+            z = torch.matmul(P,q)
+            u = torch.matmul(A, z)
+            h = torch.matmul(Q[:,:j+1].t(),u)
+            u -= torch.matmul(Q[:,:j+1],h)
+            H[:j+1,j] = h
+            H[j+1,j] = torch.linalg.norm(u)
+            q = u / H[j+1,j]
+            Q[:,j+1] = q
+            iters+=1
+            if logging:
+                y = torch.linalg.lstsq(H, e).solution
+                res_norm = torch.linalg.norm(torch.matmul(H,y) - e)
+                print(f'iter {iters}\tres = {res_norm.item()}',flush=True)
+        
+        y = torch.linalg.lstsq(H, e).solution
+        res_norm = torch.linalg.norm(torch.matmul(H,y) - e)
+        x += torch.matmul(Q[:,:j+1],y)
+
+
+    # From https://acme.byu.edu/00000179-aa18-d402-af7f-abf806ac0001/gmres2020-pdf 17.1
+    """
     res0 = b - torch.matmul(A,x0)
     res0_norm = torch.linalg.norm(res0)
     if res0_norm <= tol:
-        return x, res0_norm, 0
+        return x0, res0_norm, 0
     
     Q[:, 0] = res0 / res0_norm
     e[0] = res0_norm
@@ -195,13 +232,14 @@ def gmres(A, b, x0=None, P=None, tol=1e-5, max_iter=100, restart=None):
         y = torch.linalg.lstsq(H[:j+2,:j+1], e[:j+2]).solution
         res = torch.linalg.norm(torch.matmul(H[:j+2,:j+1],y) - e[:j+2])
         print(res.item())
+        x = torch.matmul(Q[:,:j+1], y) + x0
         #y = torch.linalg.lstsq(H, res0_norm*e).solution
         #res = torch.linalg.norm(torch.matmul(H,y) - res0_norm*e)
         iters+=1
         if res <= tol:
             break
-
-    return torch.matmul(Q[:,:j+1], y) + x0, res, iters
+    """
+    return x, res_norm, iters
 
 
 # Make system of equations
@@ -226,25 +264,49 @@ def make_system(A, P, x0, b):
             raise ValueError('matrix and preconditioner have different shapes')
         if P.dtype != dtype: P = P.type(dtype)
         if P.device != device: P.to(device)
-    return A, x, b
+    return A, P, x, b
+
+
+# Callback for scipy GMRES
+class scipy_gmres_callback(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+    def __call__(self, rk=None):
+        self.niter += 1
+        if self._disp:
+            print(f'iter {self.niter}\tres = {rk}')
 
 
 # Check GMRES implementation
-def check_gmres(N=10):
+def check_gmres(N=10, device='cpu'):
     """Compare GMRES implementation to known solution or to scipy solution
     """
-    #A = np.array([[1,0,0],[0,2,0],[0,0,3]]).astype(np.float64)
-    #b = np.array([1, 4, 6]).astype(np.float64)
+    torch_device = torch.device(device)
     A = np.random.rand(N, N).astype(np.float64)
     b = np.random.rand(N).astype(np.float64)
-    x, res, iters = gmres(torch.from_numpy(A), torch.from_numpy(b), tol=1e-5)
-    print(x)
-    print(f'Native GMRES completed with {iters} iters and residual {res.item()}')
-    x_sp, info = spla.gmres(A, b, rtol=1e-5)
+    
+    # Native
+    rtime = perf_counter()
+    x, res, iters = gmres(
+                          torch.from_numpy(A).to(torch_device),
+                          torch.from_numpy(b).to(torch_device),
+                          tol=1e-6,
+                          logging=True
+    )
+    rtime = perf_counter() - rtime
+    print(f'Native GMRES completed in {rtime} sec with {iters} iters and residual {res.item()}')
+    
+    # Scipy
+    callback = scipy_gmres_callback()
+    rtime = perf_counter()
+    x_sp, info = spla.gmres(A, b, restart=100, atol=1e-6, callback=callback)
+    rtime = perf_counter() - rtime
     if info == 0:
-        print('Scipy GMRES converged successfully')
+        print(f'Scipy GMRES converged successfully in {rtime} sec')
     else:
-        print('Scipy GMRES completed in {info} iters')
+        print(f'Scipy GMRES completed in {rtime} sec with {info} iters')
+    
     all_close = np.allclose(x.cpu().numpy(), x_sp)
     if all_close:
         print('Success')
@@ -253,9 +315,10 @@ def check_gmres(N=10):
 
 
 if __name__=='__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 3:
         N = int(sys.argv[1])
-        check_gmres(N)
+        device = sys.argv[2]
+        check_gmres(N, device)
     else:
         check_gmres()
 
