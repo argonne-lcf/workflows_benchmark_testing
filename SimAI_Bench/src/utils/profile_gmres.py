2bd7dd0805558d1f17594bb38b65036115d29bd6
balin <riccardo.balin@colorado.edu>
2024-11-06 18:36:30 +0000

    Fixed issue with profiler for gmres

--- SimAI_Bench/src/utils/profile_gmres.py
+++ SimAI_Bench/src/utils/profile_gmres.py
@@ -19,6 +19,15 @@ if __name__ == "__main__":
         device = sys.argv[2]
     torch_device = torch.device(device)
 
+    A = torch.randn(N, N, device=torch_device, dtype=torch.float64)
+    b = torch.randn(N, device=torch_device, dtype=torch.float64)
+    x, res, iters = gmres(
+                          A, b,
+                          tol=1e-7,
+                          max_iter=max_iter,
+                          restart=min(N,restart)
+    )
+
     skip = 1
     wait = 0
     warmup = 1
@@ -28,18 +37,19 @@ if __name__ == "__main__":
         activities.append(ProfilerActivity.CUDA)
     elif device=='xpu':
         activities.append(ProfilerActivity.XPU)
-    with profile(
-                activities=activities,
-                schedule=torch.profiler.schedule(
-                    skip_first=skip,
-                    wait=wait,
-                    warmup=warmup,
-                    active=active),
-                record_shapes=True,
-                profile_memory=True,
-                with_stack=True,
-                #on_trace_ready=trace_handler
-        ) as prof:
+    #with profile(
+    #            activities=activities,
+    #            schedule=torch.profiler.schedule(
+    #                skip_first=skip,
+    #                wait=wait,
+    #                warmup=warmup,
+    #                active=active),
+    #            record_shapes=True,
+    #            profile_memory=True,
+    #            with_stack=True,
+    #            #on_trace_ready=trace_handler
+    #    ) as prof:
+    with profile(activities=activities, record_shapes=True, profile_memory=True, with_stack=True) as prof:
 
         A = torch.randn(N, N, device=torch_device, dtype=torch.float64)
         b = torch.randn(N, device=torch_device, dtype=torch.float64)
@@ -50,18 +60,13 @@ if __name__ == "__main__":
                           restart=min(N,restart)
         )
 
-        prof.step()
+        #prof.step()
 
       
-    #print(prof.key_averages().table(sort_by=f"self_{device}_time_total", row_limit=20))
-    print(prof.key_averages().table())
-    if device=='cuda':
-        print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=20))
-    elif device=='xpu':
-        print(prof.key_averages().table(sort_by="self_xpu_time_total", row_limit=20))
-    else:
-        print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=20))
-    torch.save(prof.key_averages(), f'gmres_{device}_profile.tar')
+    print(prof.key_averages().table(sort_by=f"self_cpu_time_total", row_limit=10))
+    print("\n\n\n")
+    print(prof.key_averages().table(sort_by=f"self_{device}_time_total", row_limit=10))
+    torch.save(prof.key_averages(), f'gmres_{device}_profile.pt')
     prof.export_chrome_trace(f"gmres_{device}_trace.json")
 
 
