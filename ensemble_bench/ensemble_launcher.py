51789ac8fb883e895e26404bb4dc3143f1999420
Christine Simpson <csimpson@aurora-uan-0009.hostmgmt.cm.aurora.alcf.anl.gov>
2024-11-08 04:16:54 +0000

    got ensemble launcher working

--- ensemble_bench/ensemble_launcher.py
+++ ensemble_bench/ensemble_launcher.py
@@ -2,6 +2,7 @@ import os
 import socket
 import subprocess
 import time
+from pathlib import Path
 
 def launch_task(task,assigned_nodes):
     print(f"Launching task on nodes {assigned_nodes}")
@@ -10,7 +11,7 @@ def launch_task(task,assigned_nodes):
     ppn = task["ranks_per_node"]
     nranks = num_nodes*ppn
 
-    cmd = task["cmd"]
+    task_cmd = task["cmd"]
     run_dir = task["run_dir"]
 
     hosts_str = ""
@@ -20,22 +21,47 @@ def launch_task(task,assigned_nodes):
         hosts_str += node
 
     os.makedirs(run_dir, exist_ok=True)
-    cmd = f"SECONDS=0 & cd {run_dir} & mpiexec -n {nranks} --ppn {ppn} --hosts {hosts_str} {mpi_options} {cmd} >> {run_dir}/job.out & echo $SECONDS"
-    p = subprocess.Popen(cmd, shell=True)
+
+    cmd = f"mpiexec -n {nranks} --ppn {ppn} --hosts {hosts_str} {mpi_options} {task_cmd}"
+
+    print(cmd)
+    p = subprocess.Popen(cmd,
+                         executable="/bin/bash",
+                         shell=True,
+                         stdout=open(os.path.join(run_dir,'job.out'),'wb'),
+                         stderr=subprocess.STDOUT,
+                         stdin=subprocess.DEVNULL,
+                         cwd=run_dir,
+                         env=os.environ.copy(),)
     
     return p
 start_time = time.perf_counter()
-ntasks = 10
+
+# Get available nodes
+free_node_list = []
+node_file = os.getenv("PBS_NODEFILE")
+with open(node_file,"r") as f:
+    free_node_list = f.readlines()
+    free_node_list = [node.split("\n")[0] for node in free_node_list]
+total_job_nodes = len(free_node_list)
+
+
+ntasks = (total_job_nodes//2) * 4
 NX = 400
 gpu_affinity = "/soft/tools/mpi_wrapper_utils/gpu_tile_compact.sh"
 executable = "$HOME/bin/lmp_aurora_kokkos"
 inputs = f"-in $HOME/bin/lj_lammps_template.in -k on g 1 -var nx {NX} -sf kk -pk kokkos neigh half neigh/qeq full newton on"
+
+#executable = "sleep"
+#inputs = "10"
 
 num_nodes = 2
 ranks_per_node =  12
 NDEPTH = 8
 NTHREADS = 1
 mpi_options = f"--depth={NDEPTH} --cpu-bind depth --env OMP_NUM_THREADS={NTHREADS} --env OMP_PROC_BIND=spread --env OMP_PLACES=cores"
+#mpi_options = ""
+
 
 print(f'Launching node is {socket.gethostname()}')
 
@@ -50,13 +76,7 @@ for i in range(ntasks):
                       'run_dir': os.getcwd()+f"/outputs/{i}",
                       'status': 'ready'}
 
-# Get available nodes
-free_node_list = []
-node_file = os.getenv("PBS_NODEFILE")
-with open(node_file,"r") as f:
-    free_node_list = f.readlines()
-    free_node_list = [node.split("\n")[0] for node in free_node_list]
-total_job_nodes = len(free_node_list)
+
 
 ready_tasks_id = list(tasks.keys())
 running_tasks_id = []
@@ -92,7 +112,6 @@ while num_unfinished_tasks > 0:
                 tasks[tid]["status"] = "finished"
             else:
                 tasks[tid]["status"] = "failed"
-            print(f"{popen_proc.stdout=}")
             running_tasks_id.remove(tid)
             assigned_nodes = launched_procs[tid]["assigned_nodes"]
             for node in assigned_nodes:
